ad8d70e714ce45225a7edf7a33f990d6b25e2096
ark1987s <arkadiy1987@yandex.ru>
2022-07-03 01:40:21 +0300

    update

--- ITMO.CSCourse2022.WFApp.Lab01.Ex02/Form1.cs
+++ ITMO.CSCourse2022.WFApp.Lab01.Ex02/Form1.cs
@@ -8,7 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace ITMO.CSCourse2022.WFApp.Lab01.Ex01
+namespace ITMO.CSCourse2022.WFApp.Lab01.Ex02
 {
     public partial class Form1 : Form
     {
@@ -19,22 +19,18 @@ namespace ITMO.CSCourse2022.WFApp.Lab01.Ex01
 
         private void button1_Click(object sender, EventArgs e)
         {
-            this.FormBorderStyle = FormBorderStyle.Sizable;
-        }
-
-        private void button2_Click(object sender, EventArgs e)
-        {
-            this.Size = new Size(300, 500);
-        }
-
-        private void button3_Click(object sender, EventArgs e)
-        {
-            this.Opacity = 1;
+            this.Close();
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
-
+            System.Drawing.Drawing2D.GraphicsPath myPath =
+                new System.Drawing.Drawing2D.GraphicsPath();
+            myPath.AddPolygon(new Point[] { new Point(0, 0),
+                new Point(0, this.Height),
+                new Point(this.Width, 0) });
+            Region myRegion = new Region(myPath);
+            this.Region = myRegion;
         }
     }
 }
