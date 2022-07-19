17a29964e5c30674bfcc74f461df50465dff65af
ark1987s <arkadiy1987@yandex.ru>
2022-07-19 23:28:47 +0300

    new

---
+++ ITMO.CSCourse2022.WFApp.Lab02.Ex06_BiblWorm/Bibl/IPubs.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ITMO.CSCourse2022.WFApp.Lab02.Ex06_BiblWorm
+{
+    interface IPubs
+    {
+        
+       void Subs ();
+                
+    }
+}
